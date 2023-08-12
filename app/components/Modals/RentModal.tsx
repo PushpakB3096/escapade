eb88ce5dbb7db8464e74cc648606625b4a0558bb
Pushpak Bhattacharya <rtpushpak@gmail.com>
2023-08-12 10:55:27 +0530

    feat: add image upload support

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../Inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../Inputs/Counter';
+import ImageUpload from '../Inputs/ImageUpload';
 
 interface RentModalProps {}
 
@@ -53,6 +54,7 @@ const RentModal: React.FC<RentModalProps> = ({}) => {
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
+  const imageSrc = watch('imageSrc');
 
   // directly importing Map was causing issues with SSR sometimes
   const Map = useMemo(
@@ -169,6 +171,22 @@ const RentModal: React.FC<RentModalProps> = ({}) => {
           subtitle='How many bathrooms do you have?'
           value={bathroomCount}
           onChange={value => setCustomValue('bathroomCount', value)}
+        />
+      </div>
+    );
+  }
+
+  if (currentStep === STEPS_FOR_RENTING.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Add a photo of your place'
+          subtitle='Show guests what your place looks like!'
+          center
+        />
+        <ImageUpload
+          onChange={value => setCustomValue('imageSrc', value)}
+          value={imageSrc}
         />
       </div>
     );
