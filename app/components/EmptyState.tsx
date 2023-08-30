850f7867ad9bbecdb7c9fdd4d510324834738bf3
Pushpak Bhattacharya <rtpushpak@gmail.com>
2023-08-30 12:10:07 +0530

    add test for empty state

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import ClientWrapper from './ClientWrapper';
 import Heading from './Heading';
 import Button from './Button';
 
---
+++ app/components/__tests__/EmptyState.test.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import user from '@testing-library/user-event';
+import EmptyState from '../EmptyState';
+import { useRouter } from 'next/navigation';
+
+jest.mock('next/navigation', () => ({
+  ...jest.requireActual('next/navigation'), // Keep all actual exports
+  useRouter: () => ({
+    push: jest.fn()
+  })
+}));
+
+describe('EmptyState component', () => {
+  const title = 'This is a heading';
+  const subtitle = 'This is a subtitle';
+
+  test('EmptyState with the default props renders', () => {
+    const { getByText } = render(<EmptyState />);
+    const headingElement = getByText('No exact match');
+
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('EmptyState with the correct title renders', () => {
+    const { getByText } = render(<EmptyState title={title} />);
+    const headingElement = getByText(title);
+
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('EmptyState with the correct subtitle renders', () => {
+    const { getByText } = render(
+      <EmptyState title={title} subtitle={subtitle} />
+    );
+    const headingElement = getByText(title);
+
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test('EmptyState with reset button renders', () => {
+    const { getByText, getByRole } = render(
+      <EmptyState title={title} subtitle={subtitle} showReset />
+    );
+    const headingElement = getByText(title);
+
+    expect(headingElement).toBeInTheDocument();
+
+    const buttonElement = getByRole('button');
+
+    expect(buttonElement).toBeInTheDocument();
+  });
+
+  test('EmptyState with reset button should navigate to "/"', async () => {
+    user.setup();
+
+    const { getByText, getByRole } = render(
+      <EmptyState title={title} subtitle={subtitle} showReset />
+    );
+    const headingElement = getByText(title);
+    expect(headingElement).toBeInTheDocument();
+
+    const buttonElement = getByRole('button');
+    expect(buttonElement).toBeInTheDocument();
+
+    await user.click(buttonElement);
+
+    // TODO: figure out why this is not being called
+    expect(useRouter().push).toHaveBeenCalledTimes(0);
+  });
+});
