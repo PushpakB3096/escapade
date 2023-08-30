04c79de7849c732e95483b4b48c29ecef24321a2
Pushpak Bhattacharya <rtpushpak@gmail.com>
2023-08-30 14:37:20 +0530

    Add tests for listinginfo component

--- app/components/Listings/ListingInfo.tsx
+++ app/components/Listings/ListingInfo.tsx
@@ -34,7 +34,7 @@ const ListingInfo: React.FC<ListingInfoProps> = ({
   const coordinates = getByValue(locationValue)?.latlng;
 
   return (
-    <div className='col-span-4 gap-8 flex flex-col'>
+    <div className='col-span-4 gap-8 flex flex-col' data-testid='listing-info'>
       <div className='flex flex-col gap-2'>
         <div className='text-xl font-semibold flex flex-row items-center gap-2'>
           <div>Hosted by {user?.name}</div>
---
+++ app/components/__tests__/ListingInfo.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, waitFor } from '@testing-library/react';
+import ListingInfo from '../Listings/ListingInfo';
+
+describe('ListingInfo component', () => {
+  test('renders by default', () => {
+    const description = 'This is a description';
+    const guestCount = 2;
+    const roomCount = 2;
+    const bathroomCount = 1;
+    const mockUser = {
+      name: 'Pushpak Bhattacharya',
+      email: 'rtpushpak@gmail.com',
+      image: '/test',
+      id: '',
+      hashedPassword: '',
+      favoriteIds: [],
+      createdAt: '',
+      updatedAt: '',
+      emailVerified: ''
+    };
+
+    const { getByTestId } = render(
+      <ListingInfo
+        user={mockUser}
+        description={description}
+        guestCount={guestCount}
+        roomCount={roomCount}
+        bathroomCount={bathroomCount}
+        locationValue={''}
+        category={undefined}
+      />
+    );
+
+    const listingElement = getByTestId('listing-info');
+
+    waitFor(() => {
+      expect(listingElement).toBeInTheDocument();
+    });
+  });
+});
